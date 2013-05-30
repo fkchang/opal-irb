1868840fe5a7f0d943c6d29af2553aea43ec0d6f
Forrest Chang <fchang@hedgeye.com>
2013-05-29 17:17:52 -0700

    add ctl-p, ctl-n

--- opal_repl.rb
+++ opal_repl.rb
@@ -204,14 +204,14 @@ Document.ready? do
         @multiline = ! @multiline
         setPrompt()
 
-      when 38                   # up arrow
+      when 38, 80               # up arrow, ctl-p
         e.prevent_default
 
         if @historyi < @history.length-1
           @historyi += 1
           @input.value =  @history[@historyi]
         end
-      when 40                   # down arrow
+      when 40, 78               # down arrow, ctl-n
         e.prevent_default
 
         if @historyi > 0
